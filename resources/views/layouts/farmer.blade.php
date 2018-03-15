d45c457659f5f321849f8e9cfe05f1de887cdf6c
biankp <biankaperezherrera@gmail.com>
2018-03-15 14:04:26 -0600

    cambios
    agregue barra a la ventana de inspectores y productores

---
+++ resources/views/layouts/farmer.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('admin-options')
+    <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+            <i class="fa fa-pencil fa-fw"></i>Ver<span class="caret"></span>
+        </a>
+        <ul class="dropdown-menu" role="menu">
+            <li><a href="{{route('farmer.fincas.index')}}"><i class="fa fa-user fa-fw"></i>Fincas</a></li>
+        </ul>
+    </li>
+@endsection
+@section('content')
+@yield('page')
+@endsection
