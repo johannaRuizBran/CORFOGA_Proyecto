a23a884ada542421c14d6635d1455f9249222445
Johanna Ruiz <rbjoha@gmail.com>
2018-04-29 15:58:21 -0600

    xsaxas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">Correo electrónico</label>
+                            <label for="email" class="col-md-4 control-label">Correo electróxxxnico</label>
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
                                 @if ($errors->has('email'))
@@ -21,7 +21,7 @@
                             </div>
                         </div>
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Contraseña</label>
+                            <label for="password" class="col-md-4 control-label">Contzaszraseña</label>
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
                                 @if ($errors->has('password'))
